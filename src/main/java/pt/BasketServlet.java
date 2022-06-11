c12c6a14534eddd09656b00d73f225fe10c66d26
patotskay <patotskay@gmail.com>
2022-06-11 19:53:09 +0300

    cleaning

--- src/main/java/pt/BasketServlet.java
+++ src/main/java/pt/BasketServlet.java
@@ -21,10 +21,7 @@ public class BasketServlet extends HttpServlet {
     public static final String HTML_END = "</body></html>";
     public static final String HTML_RETURN = "<p><a href=\"./\">Back</a</p>";
     public static final String NAME = "name";
-    public static final String AUTHOR = "author";
-    public static final String PRICE = "price";
-    public static final String CATEGORY = "category";
-    public static final String NUMBER_OF_PAGES = "numberOfPages";
+
     private Map<String, Book> books = null;
 
     @Override
@@ -38,42 +35,21 @@ public class BasketServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException {
 
-
-
         String bName = request.getParameter(NAME);
-
-
-
         Book sb = books.get(bName);
-
-
         Object mb = request.getSession().getAttribute("MyBasket");
 
-       /* List<Book> list = new ArrayList<>();
-        list.add(sb);
-        request.getSession().setAttribute("MyBasket", list);
-*/
         if (mb != null) {
             List<Book> list = (List<Book>) mb;
             list.add(sb);
-
-            System.out.println(">>>>>>>>>>>>>"+ list);
         } else {
             List<Book> list = new ArrayList<>();
             list.add(sb);
             request.getSession().setAttribute("MyBasket", list);
-            System.out.println(">>>>>>>>>>>>>"+ list);
-
         }
         response.getWriter().append(HTML_BEGIN);
         response.getWriter().append("Book added!");
         response.getWriter().append(HTML_RETURN);
         response.getWriter().append(HTML_END);
-
-
     }
-
-
-
-
 }
